65f8176caec162cb262d6a2c1d3c82bcfe32b551
Antonio Vasilev <antoniovassilev@gmail.com>
2015-11-16 14:03:09 +0000

    Test Commit
    Test123

--- js/core.js
+++ js/core.js
@@ -1,5 +1,23 @@
 'use strict';
 
 angular.module('myApp', []) //declare my application as myApp
+//Declare the controller that I will be working with - pass the services which I will be using
+  .controller('searchController', function($scope, $http) { 
+    var pendingTask;
+
+    //If the model is blank then populate
+    if($scope.search === undefined){
+      $scope.search = "Wimbledon";
+      fetch();
+    }
+
+    //Listen on change 
+    $scope.change = function(){
+      if(pendingTask){
+        clearTimeout(pendingTask);
+      }
+      //TImeout to 800ms to avoid too many requests
+      pendingTask = setTimeout(fetch, 800);
+    };
 
   });
